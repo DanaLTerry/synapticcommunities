35d959c4f6ca6958b49624ce18d6bdde26503296
Dana <dlterry14@gmail.com>
2018-09-17 16:13:16 -0400

    added static pages, images, favicon, routes.rb, static_controller, and css

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,33 @@
+class StaticController < ApplicationController
+
+    def home
+    end
+
+    def medical
+    end
+
+    def dental
+    end
+
+    def education
+    end
+
+    def team
+    end
+
+    def FAQ
+    end
+
+    def COC
+    end
+
+    def payment
+    end
+
+    def registration
+
+    end
+
+    def trips
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,26 @@
 Rails.application.routes.draw do
+  root to: 'static#home'
+  get 'home' => 'static#home'
+  get 'medical' => 'static#medical'
+  get 'dental' => 'static#dental'
+  get 'education' => 'static#education'
+  get 'team' => 'static#team'
+  get 'faq' => 'static#FAQ'
+  get 'coc' => 'static#COC'
+  get 'payment' => 'static#payment'
+  get 'registration' => 'static#registration'
+  # gerat 'trips' => 'static#trips'
+
+# resources :states, only: :index
+
+
+#   get 'registration/index'
+#   root to: 'trips#index'
+#   resources :states, only: :index
+#   resources :trips do
+#     resources :registration
+#   end 
+
 end
+
+
